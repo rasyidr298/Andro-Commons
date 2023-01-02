bb8b190c734a66306c08c7b5e3514c5168babc08
Rasyid <Rasyidridla298@gmail.com>
2023-01-02 23:53:46 +0700

    fix view extentions

--- commons/src/main/java/id/rrdev/commons/extentions/View.kt
+++ commons/src/main/java/id/rrdev/commons/extentions/View.kt
@@ -4,24 +4,21 @@ import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 
-class View {
+//snacbar
+fun View.snackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_LONG).also { snackbar ->
+        snackbar.setAction("OK") {
+            snackbar.dismiss()
+        }
+    }.show()
+}
 
-    //snacbar
-    fun View.snackbar(message: String) {
-        Snackbar.make(this, message, Snackbar.LENGTH_LONG).also { snackbar ->
-            snackbar.setAction("OK") {
-                snackbar.dismiss()
-            }
-        }.show()
-    }
+//show view
+fun View.show() {
+    visibility = View.VISIBLE
+}
 
-    //show view
-    fun View.show() {
-        visibility = View.VISIBLE
-    }
-
-    //hide view
-    fun View.hide() {
-        visibility = View.GONE
-    }
+//hide view
+fun View.hide() {
+    visibility = View.GONE
 }
